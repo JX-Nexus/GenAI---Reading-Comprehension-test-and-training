f20d1e7616b31ccbb8cd4a769f98156694bf7cd6
Jayesh <jayeshkathale24@gmail.com>
2024-07-22 16:25:07 +0530

    Routing

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,8 +3,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: []}])
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <RouterProvider router={router} />
+  </Provider>,
 )
